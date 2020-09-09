c0d8bdfafc33d9cd4c47d71abe9e22487ba1e0c1
Jokk3r <jokzer43@gmail.com>
2020-09-09 14:37:14 +0200

    change Footer

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -3,12 +3,12 @@ import logo from "../../../assets/icons/black-dot.png";
 import "./footer.css";
 
 function Footer() {
-	return (
-        <footer className="Footer">
-            <img className="footer__img" src={logo} alt="logo"/>
-			<h2 className="footer__h1">Clean Code</h2>
-		</footer>
-	);
+  return (
+    <footer className="Footer">
+      <img className="footer__img" src={logo} alt="logo" />
+      <h2 className="footer__h1">Clean Code Footer</h2>
+    </footer>
+  );
 }
 
 export default Footer;
