73c8ba969471d71a23d722a7b7c90efa31689305
Gael <src.gael@gmail.com>
2020-09-09 12:58:44 +0200

    firebase get try

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,9 +8,9 @@ function App() {
 	return (
 		<div className="App">
 			{/* HEADER */}
-      <Header />
-      
-      {/* HEADER */}
+			<Header />
+
+			{/* HEADER */}
 			<Main />
 
 			{/* FOOTER */}
