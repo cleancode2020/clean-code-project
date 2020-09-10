9e9bd3a4bcbfc54f62655955bc781759d44b7e34
melkew <melad.kiwan.21@gmail.com>
2020-09-10 13:50:08 +0200

    first header structure

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -2,11 +2,35 @@ import React from "react";
 import logo from "../../../assets/icons/black-dot.png";
 import "./header.css";
 
+
+
 function Header() {
 	return (
         <header className="Header">
+			<nav className = "header__navbar">
+			{/* LOGO */}
             <img className="header__img" src={logo} alt="logo"/>
-			<h1 className="header__h1">Clean Code</h1>
+			<p className="header__p">Clean Code</p> 
+			 
+		
+
+			{/* NAVBAR */}
+			<ul className ="header__ul">
+				<li className="header__about"> About</li>
+				<li className="header__contact" >Contact</li>
+			</ul>
+
+			{/* SEARCH */}
+			
+			<form className="header__form">
+			<input type="text" value="" placeholder="Search..."/> 	
+			<button type="submit" value=""></button>
+			</form>
+			
+			</nav>
+				
+				{/* LOGIN/LOGOUT */}
+				
 		</header>
 	);
 }
