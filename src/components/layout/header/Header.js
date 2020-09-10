e67d4e509a6a0c8920d5066c9878de1fa3c15fb1
Jokk3r <jokzer43@gmail.com>
2020-09-10 16:05:10 +0200

    login and logout done

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,10 +1,9 @@
 import React from "react";
 import logo from "../../../assets/icons/black-dot.png";
 import "./header.css";
-
-// import LogIn from "../auth/Login";
+import LogIn from "../../authentification/LogIn";
 import SignUp from "../../authentification/SignUp";
-// import LogOut from "../auth/LogOut";
+import LogOut from "../../authentification/LogOut";
 
 function Header(props) {
   return (
@@ -12,36 +11,27 @@ function Header(props) {
       <img className="header__img" src={logo} alt="logo" />
       <h1 className="header__h1">Clean Code</h1>
 
-      <SignUp
-        fetchFirebase={props.fetchFirebase}
-        openModal={props.openModal}
-        closeModal={props.closeModal}
-        modalIsOpen={props.modalIsOpen}
-        setUser={props.setUser}
-        firebase={props.firebase}
-        user={props.user}
-      />
-
       {/* HEADER DIV RIGHT */}
       {/* HEADER LOGOUT OR LOGIN/SIGN UP*/}
-      {/* <div className="header-sub-div header-sub-div-right">
-				{props.user ? (
-					<>
-						<LogOut setUser={props.setUser} />
-					</>
-				) : (
-					<>
-						<LogIn
-							setUser={props.setUser}
-							fetchFirebase={props.fetchFirebase}
-						/>
-						<SignUp
-							setUser={props.setUser}
-							fetchFirebase={props.fetchFirebase}
-						/>
-					</>
-				)}
-			</div> */}
+      {props.user ? (
+        <>
+          <LogOut setUser={props.setUser} />
+        </>
+      ) : (
+        <>
+          <LogIn fetchFirebase={props.fetchFirebase} setUser={props.setUser} />
+
+          <SignUp
+            fetchFirebase={props.fetchFirebase}
+            openModal={props.openModal}
+            closeModal={props.closeModal}
+            modalIsOpen={props.modalIsOpen}
+            setUser={props.setUser}
+            firebase={props.firebase}
+            user={props.user}
+          />
+        </>
+      )}
     </header>
   );
 }
