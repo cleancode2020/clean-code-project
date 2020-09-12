63fc5470e90169549abc988c76b63b64f5e8d1c6
Gael <src.gael@gmail.com>
2020-09-12 13:15:34 +0200

    signup and login forms css updates

--- src/components/authentification/LogIn.js
+++ src/components/authentification/LogIn.js
@@ -3,58 +3,58 @@ import Modal from "react-modal";
 import LoginForm from "./LogInForm";
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modalIsOpen: false,
-    };
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalIsOpen: false,
+		};
+		this.openModal = this.openModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+	}
 
-  // OPEN TAB MODAL
-  openModal() {
-    this.setState({
-      modalIsOpen: true,
-    });
-  }
+	// OPEN TAB MODAL
+	openModal() {
+		this.setState({
+			modalIsOpen: true,
+		});
+	}
 
-  // CLOSE TAB MODAL
-  closeModal() {
-    this.setState({
-      modalIsOpen: false,
-    });
-  }
-  render() {
-    return (
-      <>
-        {/* LOGIN OPEN MODAL BUTTON */}
-        <button
-          className="modal__button modal__button__login"
-          onClick={this.openModal}
-        >
-          LOG IN
-        </button>
+	// CLOSE TAB MODAL
+	closeModal() {
+		this.setState({
+			modalIsOpen: false,
+		});
+	}
+	render() {
+		return (
+			<>
+				{/* LOGIN OPEN MODAL BUTTON */}
+				<button
+					className="modal__button modal__button__login"
+					onClick={this.openModal}
+				>
+					LOG IN
+				</button>
 
-        {/* LOGIN MODAL */}
-        <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal}>
-          {/* LOGIN CLOSE MODAL BUTTON */}
-          <button
-            className="modal__button  modal__button__close"
-            onClick={this.closeModal}
-          >
-            x
-          </button>
+				{/* LOGIN MODAL */}
+				<Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal}>
+					{/* LOGIN CLOSE MODAL BUTTON */}
+					<button
+						className="modal__button  modal__button__close"
+						onClick={this.closeModal}
+					>
+						x
+					</button>
 
-          {/* LOGIN FORM COMPONENT */}
-          <LoginForm
-            setUser={this.props.setUser}
-            fetchFirebase={this.props.fetchFirebase}
-          />
-        </Modal>
-      </>
-    );
-  }
+					{/* LOGIN FORM COMPONENT */}
+					<LoginForm
+						setUser={this.props.setUser}
+						fetchFirebase={this.props.fetchFirebase}
+					/>
+				</Modal>
+			</>
+		);
+	}
 }
 
 export default Login;
--- src/components/authentification/SignUpForm.js
+++ src/components/authentification/SignUpForm.js
@@ -3,283 +3,283 @@ import React from "react";
 // import "./form.css";
 
 class SignUpForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      userName: "",
-      password: "",
-      confirmedPassword: "",
-      error: null,
-      confirmMail: null,
-    };
-    this.input = React.createRef();
-    this.handleChangeMail = this.handleChangeMail.bind(this);
-    this.handleChangeUserName = this.handleChangeUserName.bind(this);
-    this.handleChangePassword = this.handleChangePassword.bind(this);
-    this.handleChangePasswordConfirm = this.handleChangePasswordConfirm.bind(
-      this
-    );
-    this.submitSignUp = this.submitSignUp.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: "",
+			userName: "",
+			password: "",
+			confirmedPassword: "",
+			error: null,
+			confirmMail: null,
+		};
+		this.input = React.createRef();
+		this.handleChangeMail = this.handleChangeMail.bind(this);
+		this.handleChangeUserName = this.handleChangeUserName.bind(this);
+		this.handleChangePassword = this.handleChangePassword.bind(this);
+		this.handleChangePasswordConfirm = this.handleChangePasswordConfirm.bind(
+			this
+		);
+		this.submitSignUp = this.submitSignUp.bind(this);
+	}
 
-  // FOCUS IN MODAL
-  componentDidMount() {
-    this.input.current.focus();
-    console.log(this.props.user);
-  }
+	// FOCUS IN MODAL
+	componentDidMount() {
+		this.input.current.focus();
+		console.log(this.props.user);
+	}
 
-  // MAIL SIGN UP SET STATE
-  handleChangeMail(event) {
-    this.setState({
-      email: event.target.value,
-    });
-  }
+	// MAIL SIGN UP SET STATE
+	handleChangeMail(event) {
+		this.setState({
+			email: event.target.value,
+		});
+	}
 
-  // USER NAME SIGN UP SET STATE
-  handleChangeUserName(event) {
-    this.setState({
-      userName: event.target.value,
-    });
-  }
+	// USER NAME SIGN UP SET STATE
+	handleChangeUserName(event) {
+		this.setState({
+			userName: event.target.value,
+		});
+	}
 
-  // PASSWORD SIGN UP SET STATE
-  handleChangePassword(event) {
-    this.setState({
-      password: event.target.value,
-    });
-  }
+	// PASSWORD SIGN UP SET STATE
+	handleChangePassword(event) {
+		this.setState({
+			password: event.target.value,
+		});
+	}
 
-  // CONFIRMED PASSWORD SIGN UP SET STATE
-  handleChangePasswordConfirm(event) {
-    this.setState({
-      confirmedPassword: event.target.value,
-    });
-  }
+	// CONFIRMED PASSWORD SIGN UP SET STATE
+	handleChangePasswordConfirm(event) {
+		this.setState({
+			confirmedPassword: event.target.value,
+		});
+	}
 
-  // SUBMIT SIGN UP
-  async submitSignUp(event) {
-    event.preventDefault();
-    // POST TO FIREBASE FUNCTION
-    const user = await this.signUpFirebase();
-    // console.log(user);
-    // VERIFY USER
-    if (user !== null) {
-      this.sendMailVerification(user);
-      this.updateFirebaseUserProfile(user);
-      // debugger;
-      await this.sendUserName(user);
-    }
+	// SUBMIT SIGN UP
+	async submitSignUp(event) {
+		event.preventDefault();
+		// POST TO FIREBASE FUNCTION
+		const user = await this.signUpFirebase();
+		// console.log(user);
+		// VERIFY USER
+		if (user !== null) {
+			this.sendMailVerification(user);
+			this.updateFirebaseUserProfile(user);
+			// debugger;
+			await this.sendUserName(user);
+		}
 
-    // FETCH AFTER POST TO REFRESH CONTENT
-    this.props.fetchFirebase();
-    this.props.closeModal();
-  }
+		// FETCH AFTER POST TO REFRESH CONTENT
+		this.props.fetchFirebase();
+		this.props.closeModal();
+	}
 
-  // FETCH  FIREBASE SIGN UP
-  async signUpFirebase() {
-    if (this.state.password !== this.state.confirmedPassword) {
-      return;
-    }
-    // debugger;
-    const requestOptions = {
-      method: "POST",
-      redirect: "follow",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // CONVERT STATE IN JSON STRING
-      body: JSON.stringify({
-        password: this.state.password,
-        email: this.state.email,
-        returnSecureToken: true,
-      }),
-    };
+	// FETCH  FIREBASE SIGN UP
+	async signUpFirebase() {
+		if (this.state.password !== this.state.confirmedPassword) {
+			return;
+		}
+		// debugger;
+		const requestOptions = {
+			method: "POST",
+			redirect: "follow",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			// CONVERT STATE IN JSON STRING
+			body: JSON.stringify({
+				password: this.state.password,
+				email: this.state.email,
+				returnSecureToken: true,
+			}),
+		};
 
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.props.firebase.apiKey}`,
-      requestOptions
-    ).then((response) => response.json());
+		const response = await fetch(
+			`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.props.firebase.apiKey}`,
+			requestOptions
+		).then((response) => response.json());
 
-    // VERIFY FIREBASE ERRORS
-    if (response.error) {
-      this.setState({
-        error: response.error,
-      });
+		// VERIFY FIREBASE ERRORS
+		if (response.error) {
+			this.setState({
+				error: response.error,
+			});
 
-      this.props.setUser(null);
+			this.props.setUser(null);
 
-      return null;
-    } else {
-      this.setState({
-        error: null,
-      });
-      // LOG IN USER WHEN WIGN UP MODAL CLOSE
-      this.props.setUser(response);
+			return null;
+		} else {
+			this.setState({
+				error: null,
+			});
+			// LOG IN USER WHEN WIGN UP MODAL CLOSE
+			this.props.setUser(response);
 
-      return response;
-    }
-  }
+			return response;
+		}
+	}
 
-  // SEND MAIL VERIFICATION
-  async sendMailVerification(user) {
-    const requestOptions = {
-      method: "POST",
-      redirect: "follow",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // CONVERT STATE IN JSON STRING
-      body: JSON.stringify({
-        requestType: "VERIFY_EMAIL",
-        idToken: user.idToken,
-      }),
-    };
-    await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${this.props.firebase.apiKey}`,
-      requestOptions
-    );
-  }
+	// SEND MAIL VERIFICATION
+	async sendMailVerification(user) {
+		const requestOptions = {
+			method: "POST",
+			redirect: "follow",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			// CONVERT STATE IN JSON STRING
+			body: JSON.stringify({
+				requestType: "VERIFY_EMAIL",
+				idToken: user.idToken,
+			}),
+		};
+		await fetch(
+			`https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${this.props.firebase.apiKey}`,
+			requestOptions
+		);
+	}
 
-  // SEND USER NAME
-  async updateFirebaseUserProfile(user) {
-    const requestOptions = {
-      method: "POST",
-      redirect: "follow",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // CONVERT STATE IN JSON STRING
-      body: JSON.stringify({
-        displayName: this.state.userName,
-        idToken: user.idToken,
-      }),
-    };
-    await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${this.props.firebase.apiKey}`,
-      requestOptions
-    );
-  }
+	// SEND USER NAME
+	async updateFirebaseUserProfile(user) {
+		const requestOptions = {
+			method: "POST",
+			redirect: "follow",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			// CONVERT STATE IN JSON STRING
+			body: JSON.stringify({
+				displayName: this.state.userName,
+				idToken: user.idToken,
+			}),
+		};
+		await fetch(
+			`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${this.props.firebase.apiKey}`,
+			requestOptions
+		);
+	}
 
-  async sendUserName(user) {
-    const databaseURL = this.props.firebase.databaseURL;
-    const localId = user.localId;
-    const idToken = user.idToken;
-    const requestOptions = {
-      method: "PUT",
-      redirect: "follow",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // CONVERT STATE IN JSON STRING	// }
-      body: JSON.stringify(this.state.userName),
-    };
-    // console.log(this.props.user.localId);
-    // console.log(this.props.user);
-    // debugger;
-    await fetch(
-      `${databaseURL}/users/${localId}/userName.json?auth=${idToken}`,
-      requestOptions
-    ).catch((error) => console.log("error:", error));
-  }
+	async sendUserName(user) {
+		const databaseURL = this.props.firebase.databaseURL;
+		const localId = user.localId;
+		const idToken = user.idToken;
+		const requestOptions = {
+			method: "PUT",
+			redirect: "follow",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			// CONVERT STATE IN JSON STRING	// }
+			body: JSON.stringify(this.state.userName),
+		};
+		// console.log(this.props.user.localId);
+		// console.log(this.props.user);
+		// debugger;
+		await fetch(
+			`${databaseURL}/users/${localId}/userName.json?auth=${idToken}`,
+			requestOptions
+		).catch((error) => console.log("error:", error));
+	}
 
-  render() {
-    return (
-      <form className="form__div" action="#">
-        {/* SIGN UP FORM LEGEND */}
-        <legend className="form__legend">SIGN UP</legend>
+	render() {
+		return (
+			<form className="form__div" action="#">
+				{/* SIGN UP FORM LEGEND */}
+				<legend className="form__legend">SIGN UP</legend>
 
-        {/* MAIL SIGN UP LABEL */}
-        <label className="form__label" htmlFor="category-title">
-          Your Mail
-        </label>
+				{/* MAIL SIGN UP LABEL */}
+				<label className="form__label" htmlFor="category-title">
+					Your Mail
+				</label>
 
-        {/* MAIL SIGN UP INPUT */}
-        <input
-          required
-          className="form__input"
-          type="text"
-          name="category-title"
-          id="category-title"
-          placeholder="my.email@ex..."
-          value={this.state.mail}
-          onChange={this.handleChangeMail}
-          ref={this.input}
-        />
+				{/* MAIL SIGN UP INPUT */}
+				<input
+					required
+					className="form__input"
+					type="text"
+					name="category-title"
+					id="category-title"
+					placeholder="..."
+					value={this.state.mail}
+					onChange={this.handleChangeMail}
+					ref={this.input}
+				/>
 
-        {/* USERNAME SIGN UP LABEL */}
-        <label className="form__label" htmlFor="category-title">
-          Choose a User Name:
-        </label>
+				{/* USERNAME SIGN UP LABEL */}
+				<label className="form__label" htmlFor="category-title">
+					Choose a User Name:
+				</label>
 
-        {/* USERNAME SIGN UP INPUT */}
-        <input
-          required
-          className="form__input"
-          type="text"
-          name="category-title"
-          id="category__title"
-          placeholder="my.email@ex..."
-          value={this.state.userName}
-          onChange={this.handleChangeUserName}
-        />
+				{/* USERNAME SIGN UP INPUT */}
+				<input
+					required
+					className="form__input"
+					type="text"
+					name="category-title"
+					id="category__title"
+					placeholder="..."
+					value={this.state.userName}
+					onChange={this.handleChangeUserName}
+				/>
 
-        {/* PASSWORD SIGN UP LABEL */}
-        <label className="form__label" htmlFor="category-title">
-          Choose a Password
-        </label>
+				{/* PASSWORD SIGN UP LABEL */}
+				<label className="form__label" htmlFor="category-title">
+					Choose a Password
+				</label>
 
-        {/* PASSWORD SIGN UP INPUT */}
-        <input
-          required
-          className="form__input"
-          type="password"
-          name="category-title"
-          id="category-title"
-          placeholder="my password..."
-          value={this.state.password}
-          onChange={this.handleChangePassword}
-        />
+				{/* PASSWORD SIGN UP INPUT */}
+				<input
+					required
+					className="form__input"
+					type="password"
+					name="category-title"
+					id="category-title"
+					placeholder="..."
+					value={this.state.password}
+					onChange={this.handleChangePassword}
+				/>
 
-        {/* CONFIRMED PASSWORD SIGN UP LABEL */}
-        <label className="form__label" htmlFor="category-title">
-          Confirm Password
-        </label>
+				{/* CONFIRMED PASSWORD SIGN UP LABEL */}
+				<label className="form__label" htmlFor="category-title">
+					Confirm Password
+				</label>
 
-        {/* CONFIRMED PASSWORD SIGN UP INPUT */}
-        <input
-          required
-          className="form__input"
-          type="password"
-          name="category-title"
-          id="category-title"
-          placeholder="my password..."
-          value={this.state.confirmedPassword}
-          onChange={this.handleChangePasswordConfirm}
-        />
+				{/* CONFIRMED PASSWORD SIGN UP INPUT */}
+				<input
+					required
+					className="form__input"
+					type="password"
+					name="category-title"
+					id="category-title"
+					placeholder="..."
+					value={this.state.confirmedPassword}
+					onChange={this.handleChangePasswordConfirm}
+				/>
 
-        {/* FIREBASE ERROR MESSAGE */}
-        {this.state.error && (
-          <div className="div__correct__length">
-            <p className="p__correct__length">
-              Error while trying to authenticate: {this.state.error.message}
-            </p>
-          </div>
-        )}
+				{/* FIREBASE ERROR MESSAGE */}
+				{this.state.error && (
+					<div className="div__correct__length">
+						<p className="p__correct__length">
+							Error while trying to authenticate: {this.state.error.message}
+						</p>
+					</div>
+				)}
 
-        {/* SUBMIT BUTTON */}
-        <button
-          className="submit__button"
-          onClick={(e) => {
-            this.submitSignUp(e);
-            alert("Please confirm your mail.");
-          }}
-        >
-          SIGN UP
-        </button>
-      </form>
-    );
-  }
+				{/* SUBMIT BUTTON */}
+				<button
+					className="submit__button"
+					onClick={(e) => {
+						this.submitSignUp(e);
+						alert("Please confirm your mail.");
+					}}
+				>
+					SIGN UP
+				</button>
+			</form>
+		);
+	}
 }
 
 export default SignUpForm;
