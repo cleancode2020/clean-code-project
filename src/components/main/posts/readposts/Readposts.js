1521b7755d285f7721b38405cd650824823e6ed4
Gael <src.gael@gmail.com>
2020-09-13 12:31:16 +0200

    commit

--- src/components/main/posts/readposts/Readposts.js
+++ src/components/main/posts/readposts/Readposts.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./readposts.css";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import Userpost from "./Userpost";
 import Codeblock from "./Codeblockread";
 
 const Readposts = () => {
@@ -9,73 +10,32 @@ const Readposts = () => {
 			<ul className="posts__ul">
 				{/* POST 1 */}
 				<li>
-					{/* LEGEND */}
-					<h3 className="posts__h3">user 1</h3>
-
-					{/* ARTICLE */}
-					<h4 className="posts__h4">article title 1</h4>
-					<p className="posts__p">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est
-						dolor harum ea cum voluptas aperiam cupiditate delectus? Ut,
-						voluptatum rem consequuntur voluptas maiores sunt praesentium quos
-						eaque, laborum sint quae itaque quibusdam odit quidem consequatur ad
-						debitis nesciunt cumque hic nisi illum eius nulla placeat tempora!
-						Consectetur, cum debitis.
-					</p>
-
-					{/* CODEBLOCK */}
-					<h4 className="posts__h4">codeblock 1</h4>
-					<span className="codeblock__span__posts">
-						<Codeblock />
-					</span>
+					<Link className="nav__link" to="/userpost">
+						<h3>username</h3>
+						<p>#js</p>
+					</Link>
 				</li>
 
-				{/* POST 2 */}
+				{/* POST 1 */}
 				<li>
-					{/* LEGEND */}
-					<h3 className="posts__h3">user 2</h3>
-
-					{/* ARTICLE */}
-					<h4 className="posts__h4">article title 2</h4>
-					<p className="posts__p">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est
-						dolor harum ea cum voluptas aperiam cupiditate delectus? Ut,
-						voluptatum rem consequuntur voluptas maiores sunt praesentium quos
-						eaque, laborum sint quae itaque quibusdam odit quidem consequatur ad
-						debitis nesciunt cumque hic nisi illum eius nulla placeat tempora!
-						Consectetur, cum debitis.
-					</p>
-
-					{/* CODEBLOCK */}
-					<h4 className="posts__h4">codeblock 2</h4>
-					<span className="codeblock__span__posts">
-						<Codeblock />
-					</span>
+					<Link className="nav__link" to="/userpost">
+						<h3>username</h3>
+						<p>#js</p>
+					</Link>
 				</li>
 
-				{/* POST 3 */}
+				{/* POST 1 */}
 				<li>
-					{/* LEGEND */}
-					<h3 className="posts__h3">user 3</h3>
-
-					{/* ARTICLE */}
-					<h4 className="posts__h4">article title 3</h4>
-					<p className="posts__p">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est
-						dolor harum ea cum voluptas aperiam cupiditate delectus? Ut,
-						voluptatum rem consequuntur voluptas maiores sunt praesentium quos
-						eaque, laborum sint quae itaque quibusdam odit quidem consequatur ad
-						debitis nesciunt cumque hic nisi illum eius nulla placeat tempora!
-						Consectetur, cum debitis.
-					</p>
-
-					{/* CODEBLOCK */}
-					<h4 className="posts__h4">codeblock 3</h4>
-					<span className="codeblock__span__posts">
-						<Codeblock />
-					</span>
+					<Link className="nav__link" to="/userpost">
+						<h3>username</h3>
+						<p>#js</p>
+					</Link>
 				</li>
 			</ul>
+
+			<Route path="/userpost">
+				<Userpost />
+			</Route>
 		</section>
 	);
 };
