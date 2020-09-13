77b71750d93782510780efa414f6e67c564e647c
Jokk3r <jokzer43@gmail.com>
2020-09-13 12:34:33 +0200

    commit before fetch

--- src/components/main/posts/readposts/Readposts.js
+++ src/components/main/posts/readposts/Readposts.js
@@ -1,83 +1,83 @@
 import React from "react";
 import "./readposts.css";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+// import { BrowserRouter, Route, Link } from "react-router-dom";
 import Codeblock from "./Codeblockread";
 
 const Readposts = () => {
-	return (
-		<section className="readposts-section">
-			<ul className="posts__ul">
-				{/* POST 1 */}
-				<li>
-					{/* LEGEND */}
-					<h3 className="posts__h3">user 1</h3>
+  return (
+    <section className="readposts-section">
+      <ul className="posts__ul">
+        {/* POST 1 */}
+        <li>
+          {/* LEGEND */}
+          <h3 className="posts__h3">user 1</h3>
 
-					{/* ARTICLE */}
-					<h4 className="posts__h4">article title 1</h4>
-					<p className="posts__p">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est
-						dolor harum ea cum voluptas aperiam cupiditate delectus? Ut,
-						voluptatum rem consequuntur voluptas maiores sunt praesentium quos
-						eaque, laborum sint quae itaque quibusdam odit quidem consequatur ad
-						debitis nesciunt cumque hic nisi illum eius nulla placeat tempora!
-						Consectetur, cum debitis.
-					</p>
+          {/* ARTICLE */}
+          <h4 className="posts__h4">article title 1</h4>
+          <p className="posts__p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est
+            dolor harum ea cum voluptas aperiam cupiditate delectus? Ut,
+            voluptatum rem consequuntur voluptas maiores sunt praesentium quos
+            eaque, laborum sint quae itaque quibusdam odit quidem consequatur ad
+            debitis nesciunt cumque hic nisi illum eius nulla placeat tempora!
+            Consectetur, cum debitis.
+          </p>
 
-					{/* CODEBLOCK */}
-					<h4 className="posts__h4">codeblock 1</h4>
-					<span className="codeblock__span__posts">
-						<Codeblock />
-					</span>
-				</li>
+          {/* CODEBLOCK */}
+          <h4 className="posts__h4">codeblock 1</h4>
+          <span className="codeblock__span__posts">
+            <Codeblock />
+          </span>
+        </li>
 
-				{/* POST 2 */}
-				<li>
-					{/* LEGEND */}
-					<h3 className="posts__h3">user 2</h3>
+        {/* POST 2 */}
+        <li>
+          {/* LEGEND */}
+          <h3 className="posts__h3">user 2</h3>
 
-					{/* ARTICLE */}
-					<h4 className="posts__h4">article title 2</h4>
-					<p className="posts__p">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est
-						dolor harum ea cum voluptas aperiam cupiditate delectus? Ut,
-						voluptatum rem consequuntur voluptas maiores sunt praesentium quos
-						eaque, laborum sint quae itaque quibusdam odit quidem consequatur ad
-						debitis nesciunt cumque hic nisi illum eius nulla placeat tempora!
-						Consectetur, cum debitis.
-					</p>
+          {/* ARTICLE */}
+          <h4 className="posts__h4">article title 2</h4>
+          <p className="posts__p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est
+            dolor harum ea cum voluptas aperiam cupiditate delectus? Ut,
+            voluptatum rem consequuntur voluptas maiores sunt praesentium quos
+            eaque, laborum sint quae itaque quibusdam odit quidem consequatur ad
+            debitis nesciunt cumque hic nisi illum eius nulla placeat tempora!
+            Consectetur, cum debitis.
+          </p>
 
-					{/* CODEBLOCK */}
-					<h4 className="posts__h4">codeblock 2</h4>
-					<span className="codeblock__span__posts">
-						<Codeblock />
-					</span>
-				</li>
+          {/* CODEBLOCK */}
+          <h4 className="posts__h4">codeblock 2</h4>
+          <span className="codeblock__span__posts">
+            <Codeblock />
+          </span>
+        </li>
 
-				{/* POST 3 */}
-				<li>
-					{/* LEGEND */}
-					<h3 className="posts__h3">user 3</h3>
+        {/* POST 3 */}
+        <li>
+          {/* LEGEND */}
+          <h3 className="posts__h3">user 3</h3>
 
-					{/* ARTICLE */}
-					<h4 className="posts__h4">article title 3</h4>
-					<p className="posts__p">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est
-						dolor harum ea cum voluptas aperiam cupiditate delectus? Ut,
-						voluptatum rem consequuntur voluptas maiores sunt praesentium quos
-						eaque, laborum sint quae itaque quibusdam odit quidem consequatur ad
-						debitis nesciunt cumque hic nisi illum eius nulla placeat tempora!
-						Consectetur, cum debitis.
-					</p>
+          {/* ARTICLE */}
+          <h4 className="posts__h4">article title 3</h4>
+          <p className="posts__p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est
+            dolor harum ea cum voluptas aperiam cupiditate delectus? Ut,
+            voluptatum rem consequuntur voluptas maiores sunt praesentium quos
+            eaque, laborum sint quae itaque quibusdam odit quidem consequatur ad
+            debitis nesciunt cumque hic nisi illum eius nulla placeat tempora!
+            Consectetur, cum debitis.
+          </p>
 
-					{/* CODEBLOCK */}
-					<h4 className="posts__h4">codeblock 3</h4>
-					<span className="codeblock__span__posts">
-						<Codeblock />
-					</span>
-				</li>
-			</ul>
-		</section>
-	);
+          {/* CODEBLOCK */}
+          <h4 className="posts__h4">codeblock 3</h4>
+          <span className="codeblock__span__posts">
+            <Codeblock />
+          </span>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Readposts;
