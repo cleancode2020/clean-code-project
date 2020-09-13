d048b5ca0e6659b84e43261dca907a31e7922566
Gael <src.gael@gmail.com>
2020-09-13 09:00:01 +0200

    codeblock test

--- src/components/main/posts/post/Codeblock.js
+++ src/components/main/posts/post/Codeblock.js
@@ -4,9 +4,52 @@ import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const Codeblock = () => {
 	const codeString = "(num) => num + 1";
+	const codeString2 = ` function createStyleObject(classNames, style) {
+		return classNames.reduce((styleObject, className) => {
+		  return {...styleObject, ...style[className]};
+		}, {});
+	  }
+	  
+	  function createClassNameString(classNames) {
+		return classNames.join(' ');
+	  }
+	  
+	  function createChildren(style, useInlineStyles) {
+		let childrenCount = 0;
+		return children => {
+		  childrenCount += 1;
+		  return children.map((child, i) => createElement({
+			node: child,
+			style,
+			useInlineStyles,
+			key:\`code-segment-\${childrenCount}-\${i}\`
+		  }));
+		}
+	  }
+	  
+	  function createElement({ node, style, useInlineStyles, key }) {
+		const { properties, type, tagName, value } = node;
+		if (type === "text") {
+		  return value;
+		} else if (tagName) {
+		  const TagName = tagName;
+		  const childrenCreator = createChildren(style, useInlineStyles);
+		  const props = (
+			useInlineStyles
+			?
+			{ style: createStyleObject(properties.className, style) }
+			:
+			{ className: createClassNameString(properties.className) }
+		  );
+		  const children = childrenCreator(node.children);
+		  return <TagName key={key} {...props}>{children}</TagName>;
+		}
+	  }`;
+
 	return (
 		<SyntaxHighlighter language="javascript" style={docco}>
-			{codeString}
+			{/* {codeString} */}
+			{codeString2}
 		</SyntaxHighlighter>
 	);
 };
