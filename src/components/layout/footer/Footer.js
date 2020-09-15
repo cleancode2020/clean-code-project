c646a5fd03c9767462c24f3469f32b342d95de47
Gael <src.gael@gmail.com>
2020-09-15 14:24:11 +0200

    merged master

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import logo from "../../../assets/icons/black-dot.png";
 import "./footer.css";
 
@@ -8,17 +8,32 @@ function Footer() {
 		<footer className="Footer">
 			<Link activeClassName="footer__active" to="/">
 				<img className="footer__img" src={logo} alt="logo" />
-				<h2 className="footer__h1">Clean Code Footer</h2>
+				<h2 className="footer__h1">cleancode</h2>
 			</Link>
 			<nav>
 				<ul>
-					<li><Link activeClassName="footer__active" to="/about">About</Link></li>
-					<li><Link activeClassName="footer__active" to="/contact">Contact</Link></li>
-					<li><Link activeClassName="footer__active" to="/privacy">Privacy</Link></li>
-					<li><Link activeClassName="footer__active" to="/impressum">Impressum</Link></li>
+					<li>
+						<Link activeClassName="footer__active" to="/about">
+							About
+						</Link>
+					</li>
+					<li>
+						<Link activeClassName="footer__active" to="/contact">
+							Contact
+						</Link>
+					</li>
+					<li>
+						<Link activeClassName="footer__active" to="/privacy">
+							Privacy
+						</Link>
+					</li>
+					<li>
+						<Link activeClassName="footer__active" to="/impressum">
+							Impressum
+						</Link>
+					</li>
 				</ul>
 			</nav>
-
 		</footer>
 	);
 }
