22ac2c21deae499e52761e22cec2f6d6311f2c05
Victoria <testversion2617@gmx.de>
2020-09-15 21:30:00 +0200

    change footer style

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -6,19 +6,20 @@ import "./footer.css";
 function Footer() {
 	return (
 		<footer className="Footer">
-			<Link className="footer__main" to="/">
-				<img className="footer__img" src={logo} alt="logo" />
-				<h2 className="footer__h1">cleancode</h2>
-			</Link>
 			<nav className="footer__nav">
 				<ul className="footer__ul">
+					<li className="footer__logo">
+						<Link className="footer__main" to="/">
+							<img className="footer__img" src={logo} alt="logo" />
+							<h2 className="footer__h1">cleancode</h2>
+						</Link>
+					</li>
 					<li><Link className="footer__name" to="/about">About</Link></li>
 					<li><Link className="footer__name" to="/contact">Contact</Link></li>
 					<li><Link className="footer__name" to="/privacy">Privacy</Link></li>
 					<li><Link className="footer__name" to="/impressum">Impressum</Link></li>
 				</ul>
 			</nav>
-
 		</footer>
 	);
 }
