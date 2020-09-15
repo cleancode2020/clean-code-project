bb2c12fea67b562f6c21669cd07cf1bb86ec3d5c
melkew <melad.kiwan.21@gmail.com>
2020-09-15 09:39:17 +0200

    struckture about component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,96 @@
 import React from "react";
-import "./about.css"
+import "../about/About.css";
+import gael from "../../assets/teamImage/gael.jpg";
+import andreas from "../../assets/teamImage/andreas.png";
+import melad from "../../assets/teamImage/melad.png";
+import victoria from "../../assets/teamImage/victoria.jpg";
+import vadim from "../../assets/teamImage/vadim.png";
 
-function About() {
-	return (
-		<div>
-			<h2>About</h2>
-		</div>
-	);
+class About extends React.Component {
+  
+  
+  state = {
+    teams: [
+      { id: 1, name: "Gael", title: "frontend-management" },
+      { id: 2, name: "Andreas", title: "frontend-management" },
+      { id: 3, name: "Melad", title: "frontend-developer" },
+      { id: 4, name: "Victoria", title: "frontend-developer" },
+      { id: 5, name: "Vadim", title: "frontend-developer" },
+    ],
+  };
+  handleOnMouseMove = () => {
+    return console.log(this.state.teams[1].title);
+  };
+
+  render() {
+    return (
+      <main>
+        <h1 className="h1__about">About Us</h1>
+
+        {/* DISCRIPTION ABOUT US*/}
+        <h2 className="h2__style">who we are</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet
+        </p>
+
+        {/* OUR PRODUCT */}
+        <h2 className="h2__style">Our Product</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet
+        </p>
+
+        {/* TEAM PICS AND SOME TEXT ABOUT THEM */}
+        <h2>Our Team</h2>
+        <div className="team__container">
+          <div>
+            <img src={gael} alt="gael" onMouseMove={this.handleOnMouseMove} />
+            <p>Gael</p>
+          </div>
+
+          <div>
+            <img src={melad} alt="melad" onMouseMove={this.handleOnMouseMove} />
+            <p>Melad</p>
+          </div>
+
+          <div>
+            <img
+              src={victoria}
+              alt="victoria"
+              onMouseMove={this.handleOnMouseMove}
+            />
+            <p>Victoria</p>
+          </div>
+
+          <div>
+            <img src={vadim} alt="vadim" onMouseMove={this.handleOnMouseMove} />
+            <p>Vadim</p>
+          </div>
+
+          <div>
+            <img
+              src={andreas}
+              alt="andreass"
+              onMouseMove={this.handleOnMouseMove}
+            />
+            <p>Andreas</p>
+          </div>
+        </div>
+		{/* CONTACT */}
+        <h2 className="h2__style">make contact with us</h2>
+        <p>
+          <a href="teamcode35@gmail.com">would you share your opinion on it to make it better </a>
+        </p>
+      </main>
+    );
+  }
 }
 
 export default About;
