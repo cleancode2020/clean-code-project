f79668bf83bd422da3a83582a3a45b6c70f08182
Victoria <testversion2617@gmx.de>
2020-09-16 20:50:38 +0200

    change privacy text

--- src/components/legal/impressum/Impressum.js
+++ src/components/legal/impressum/Impressum.js
@@ -4,7 +4,7 @@ import "./impressum.css";
 function Impressum() {
   return (
     <div className="main__impressum">
-      <h2>Impressum</h2>
+      <h2>Folgende Pflichtangaben müssen in einem Impressum enthalten sein</h2>
     </div>
   );
 }
