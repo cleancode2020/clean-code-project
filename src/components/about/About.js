98181e4a6319893e0fe629974031d899fec9d79a
melkew <melad.kiwan.21@gmail.com>
2020-09-16 12:32:06 +0200

    about component updated

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,11 +9,41 @@ import vadim from "../../assets/teamImage/Vadim.png";
 class About extends React.Component {
   state = {
     teams: [
-      { id: 1, name: "Gael", title: "frontend-management" },
-      { id: 2, name: "Andreas", title: "frontend-management" },
-      { id: 3, name: "Melad", title: "frontend-developer" },
-      { id: 4, name: "Victoria", title: "frontend-developer" },
-      { id: 5, name: "Vadim", title: "frontend-developer" },
+      {
+        id: 1,
+        name: "Gael",
+        title: "frontend-management",
+        linkedInPage: "https://www.linkedin.com/in/gael-src/",
+        github: "https://github.com/gael-src",
+      },
+      {
+        id: 2,
+        name: "Andreas",
+        title: "frontend-management",
+        linkedInPage: "",
+        github: "https://github.com/jokk3r",
+      },
+      {
+        id: 3,
+        name: "Melad",
+        title: "frontend-developer",
+        linkedInPage: "https://www.linkedin.com/in/melad-kewan-a087221b2/",
+        github: "https://github.com/melkew",
+      },
+      {
+        id: 4,
+        name: "Victoria",
+        title: "frontend-developer",
+        linkedInPage: "https://www.linkedin.com/in/victoria15/",
+        github: "https://github.com/victoria2266",
+      },
+      {
+        id: 5,
+        name: "Vadim",
+        title: "frontend-developer",
+        linkedInPage: "https://www.linkedin.com/in/vadim-eremia-6a846510a/",
+        github: "https://github.com/wadush",
+      },
     ],
   };
   handleOnMouseMove = () => {
@@ -27,7 +57,7 @@ class About extends React.Component {
 
         {/* DISCRIPTION ABOUT US*/}
         <h2 className="h2__style">who we are</h2>
-        <p>
+        <p className="paragraph">
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
           sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
@@ -37,7 +67,7 @@ class About extends React.Component {
 
         {/* OUR PRODUCT */}
         <h2 className="h2__style">Our Product</h2>
-        <p>
+        <p className="paragraph">
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
           sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
@@ -46,48 +76,84 @@ class About extends React.Component {
         </p>
 
         {/* TEAM PICS AND SOME TEXT ABOUT THEM */}
-        <h2>Our Team</h2>
+        <h2 className="h2__style">Our Team</h2>
         <div className="team__container">
-          <div>
-            <img src={gael} alt="gael" onMouseMove={this.handleOnMouseMove} />
-            <p>Gael</p>
-          </div>
+          <figure className="effect__imgs">
+            <div className="figcaption">
+              <img src={gael} alt="gael" />
+              <h2>{this.state.teams[0].name}</h2>
+              <p className="links">
+                <a href={this.state.teams[0].linkedInPage}>Linkedin</a>
+                <a href={this.state.teams[0].github}>Github</a>
+              </p>
 
-          <div>
-            <img src={melad} alt="melad" onMouseMove={this.handleOnMouseMove} />
-            <p>Melad</p>
-          </div>
+              <div className="description">
+                <p>{this.state.teams[0].name}</p>
+                <p>{this.state.teams[0].title}</p>
+              </div>
+            </div>
 
-          <div>
-            <img
-              src={victoria}
-              alt="victoria"
-              onMouseMove={this.handleOnMouseMove}
-            />
-            <p>Victoria</p>
-          </div>
+            <div className="figcaption">
+              <img src={andreas} alt="andreass" />
 
-          <div>
-            <img src={vadim} alt="vadim" onMouseMove={this.handleOnMouseMove} />
-            <p>Vadim</p>
-          </div>
+              <h2>{this.state.teams[1].name}</h2>
+              <p className="links">
+                <a href={this.state.teams[1].linkedInPage}>Linkedin</a>
+                <a href={this.state.teams[1].github}>Github</a>
+              </p>
 
-          <div>
-            <img
-              src={andreas}
-              alt="andreass"
-              onMouseMove={this.handleOnMouseMove}
-            />
-            <p>Andreas</p>
-          </div>
+              <div className="description">
+                <p>{this.state.teams[1].name}</p>
+                <p>{this.state.teams[1].title}</p>
+              </div>
+            </div>
+
+            <div className="figcaption">
+              <img src={melad} alt="melad" />
+
+              <h2>{this.state.teams[2].name}</h2>
+              <p className="links">
+                <a href={this.state.teams[2].linkedInPage}>Linkedin</a>
+                <a href={this.state.teams[2].github}>Github</a>
+              </p>
+
+              <div className="description">
+                <p>{this.state.teams[2].name}</p>
+                <p>{this.state.teams[2].title}</p>
+              </div>
+            </div>
+
+            <div className="figcaption">
+              <img src={victoria} alt="victoria" />
+
+              <h2>{this.state.teams[3].name}</h2>
+              <p className="links">
+                <a href={this.state.teams[3].linkedInPage}>Linkedin</a>
+                <a href={this.state.teams[3].github}>Github</a>
+              </p>
+
+              <div className="description">
+                <p>{this.state.teams[3].name}</p>
+                <p>{this.state.teams[3].title}</p>
+              </div>
+            </div>
+
+            <div className="figcaption">
+              <img src={vadim} alt="vadim" />
+
+              <h2>{this.state.teams[4].name}</h2>
+              <p className="links">
+                <a href={this.state.teams[4].linkedInPage}>Linkedin</a>
+                <a href={this.state.teams[4].github}>Github</a>
+              </p>
+
+              <div className="description">
+                <p>{this.state.teams[4].name}</p>
+                <p>{this.state.teams[4].title}</p>
+              </div>
+            </div>
+          </figure>
         </div>
-        {/* CONTACT */}
-        <h2 className="h2__style">make contact with us</h2>
-        <p>
-          <a href="teamcode35@gmail.com">
-            would you share your opinion on it to make it better{" "}
-          </a>
-        </p>
       </main>
     );
   }
