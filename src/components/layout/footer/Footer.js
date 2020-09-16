c088eb3735d61d5700ed7a13e6298989a06faef6
Victoria <testversion2617@gmx.de>
2020-09-16 16:29:08 +0200

    added icons to footer

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -8,18 +8,20 @@ function Footer() {
 		<footer className="Footer">
 			<nav>
 				<ul>
+				{/*
 					<li>
 						<Link className="footer__main" to="/">
 							<img className="footer__img" src={logo} alt="logo" />
 							<h2 className="footer__h1">cleancode</h2>
-						</Link>
+						</Link>						
 					</li>
-					<li><Link className="footer__name" to="/about">About</Link></li>
-					<li><Link className="footer__name" to="/contact">Contact</Link></li>
-					<li><Link className="footer__name" to="/privacy">Privacy</Link></li>
-					<li><Link className="footer__name" to="/impressum">Impressum</Link></li>
+					*/}
+					<li><Link className="footer__name" to="/about"><i class="fas fa-users icon"></i> About</Link></li>
+					<li><Link className="footer__name" to="/contact"><i class="far fa-envelope icon"></i> Contact</Link></li>
+					<li><Link className="footer__name" to="/privacy"><i class="fas fa-user-shield icon"></i> Privacy</Link></li>
+					<li><Link className="footer__name" to="/impressum"><i class="fas fa-map-marker-alt icon"></i> Impressum</Link></li>
 				</ul>
-			</nav>
+			</nav> 
 		</footer>
 	);
 }
