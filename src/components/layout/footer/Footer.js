f492e5dda43f51f1e394e6eb3343edc817825965
Gael <src.gael@gmail.com>
2020-09-17 12:53:58 +0200

    removed comments

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -8,14 +8,6 @@ function Footer() {
 		<footer className="Footer">
 			<nav>
 				<ul>
-				{/*
-					<li>
-						<Link className="footer__main" to="/">
-							<img className="footer__img" src={logo} alt="logo" />
-							<h2 className="footer__h1">cleancode</h2>
-						</Link>						
-					</li>
-					*/}
 					<li><Link className="footer__name" to="/about"><i class="fas fa-users icon"></i> About</Link></li>
 					<li><Link className="footer__name" to="/contact"><i class="far fa-envelope icon"></i> Contact</Link></li>
 					<li><Link className="footer__name" to="/privacy"><i class="fas fa-user-shield icon"></i> Privacy</Link></li>
