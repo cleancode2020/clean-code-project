e482cb30194a0ec4f37c76511b2cf2bf8e791ce0
Jokk3r <jokzer43@gmail.com>
2020-09-21 16:02:05 +0200

    comment logic beginn

--- src/components/main/posts/readposts/Readposts.js
+++ src/components/main/posts/readposts/Readposts.js
@@ -4,80 +4,82 @@ import { Route, Link } from "react-router-dom";
 import Userpost from "./Userpost";
 
 class Readposts extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			activePost: "",
-			currentPost: [],
-		};
-		this.setPost = this.setPost.bind(this);
-	}
-	componentDidMount() {
-		this.props.getFirebase();
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePost: "",
+      currentPost: [],
+    };
+    this.setPost = this.setPost.bind(this);
+  }
+  componentDidMount() {
+    this.props.getFirebase();
+  }
 
-	// ACTIVE POST
-	setPost(item) {
-		this.setState({
-			activePost: item[0],
-			currentPost: item,
-		});
-	}
+  // ACTIVE POST
+  setPost(item) {
+    this.setState({
+      activePost: item[0],
+      currentPost: item,
+    });
+  }
 
-	// ACTIVE POST
-	reloadPage() {
-		window.location.reload();
-	}
+  // ACTIVE POST
+  reloadPage() {
+    window.location.reload();
+  }
 
-	render() {
-		// DATA
-		let posts = [];
-		if (this.props.allPostsObject) {
-			const allPostsObject = this.props.allPostsObject;
-			const allPostsObjectKeys = Object.keys(allPostsObject);
-			for (let key of allPostsObjectKeys) {
-				const objectData = allPostsObject[key];
-				if (typeof objectData === "object") {
-					const postsValuesArray = Object.values(objectData);
-					// POST ALONE
-					postsValuesArray.unshift(key);
-					// POSTS
-					posts.unshift(postsValuesArray);
-				}
-			}
-		}
+  render() {
+    // DATA
+    let posts = [];
+    if (this.props.allPostsObject) {
+      const allPostsObject = this.props.allPostsObject;
+      const allPostsObjectKeys = Object.keys(allPostsObject);
+      for (let key of allPostsObjectKeys) {
+        const objectData = allPostsObject[key];
+        if (typeof objectData === "object") {
+          const postsValuesArray = Object.values(objectData);
+          // POST ALONE
+          postsValuesArray.unshift(key);
+          // POSTS
+          posts.unshift(postsValuesArray);
+        }
+      }
+    }
 
-		return (
-			<section className="readposts-section">
-				<ul className="posts__ul">
-					{this.state.activePost ? (
-						<Userpost
-							currentPost={this.state.currentPost}
-							reloadPage={this.reloadPage}
-						/>
-					) : (
-						posts.map((item, index) => (
-							<button
-								className="post__button"
-								onClick={() => this.setPost(item)}
-								to={`/${item[0]}`}
-							>
-								<li className="posts__li" key={index}>
-									<h2 className="article-h2">{item[5]}</h2>
-									<p className="article-p">{item[2]}</p>
-									<p className="article-p">{item[4]}</p>
-									<h3 className="article-h3">{item[6]}</h3>
-								</li>
-							</button>
-						))
-					)}
-					{/* POST IN FULL */}
-					{/* <Route path={`/${this.state.activePost}`}>
+    return (
+      <section className="readposts-section">
+        <ul className="posts__ul">
+          {this.state.activePost ? (
+            <Userpost
+              currentPost={this.state.currentPost}
+              reloadPage={this.reloadPage}
+              user={this.props.user}
+              firebase={this.props.firebase}
+            />
+          ) : (
+            posts.map((item, index) => (
+              <button
+                className="post__button"
+                onClick={() => this.setPost(item)}
+                to={`/${item[0]}`}
+              >
+                <li className="posts__li" key={index}>
+                  <h2 className="article-h2">{item[5]}</h2>
+                  <p className="article-p">{item[2]}</p>
+                  <p className="article-p">{item[4]}</p>
+                  <h3 className="article-h3">{item[6]}</h3>
+                </li>
+              </button>
+            ))
+          )}
+          {/* POST IN FULL */}
+          {/* <Route path={`/${this.state.activePost}`}>
 						<Userpost posts={posts} />
 					</Route> */}
 
-					{/* POSTS LIST */}
-					{/* {posts.map((item, index) => (
+          {/* POSTS LIST */}
+          {/* {posts.map((item, index) => (
 						<Link
 							className="nav__link"
 							onClick={() => this.setPost(item)}
@@ -91,10 +93,10 @@ class Readposts extends React.Component {
 							</li>
 						</Link>
 					))} */}
-				</ul>
-			</section>
-		);
-	}
+        </ul>
+      </section>
+    );
+  }
 }
 
 export default Readposts;
--- src/components/main/posts/readposts/Userpost.js
+++ src/components/main/posts/readposts/Userpost.js
@@ -2,37 +2,119 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Codeblock from "./Codeblockread";
 
-const Userpost = (props) => {
-	console.log(props.currentPost);
-	return (
-		<div className="post__div">
-			<button className="nav__link nav__close" onClick={props.reloadPage}>
-				x close
-			</button>
-			{/* <Link className="nav__link nav__close" to="/">
-				x close
-			</Link> */}
+class Userpost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: "",
+      date: "",
+      currentPost: "",
+    };
+    this.textAreaChange = this.textAreaChange.bind(this);
+    this.addComment = this.addComment.bind(this);
+  }
 
-			{/* TITLE */}
-			<h3 className="posts__h3">{props.currentPost[5]}</h3>
+  textAreaChange(e) {
+    this.setState({
+      comment: e.target.value,
+    });
+  }
+  addComment() {
+    this.postFirebase();
+  }
 
-			{/* CATEGORIES */}
-			<h4 className="posts__p">{props.currentPost[2]}</h4>
-			<h4 className="posts__p">{props.currentPost[4]}</h4>
+  async postFirebase() {
+    const requestOptions = {
+      method: "POST",
+      redirect: "follow",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // CONVERT STATE IN JSON STRING
+      body: JSON.stringify({
+        comment: this.state.comment,
+        date: new Date(),
+        currentPost: this.props.currentPost[0],
+      }),
+    };
 
-			{/* ARTICLE */}
-			<p className="posts__p">{props.currentPost[1]}</p>
+    await fetch(
+      `${this.props.firebase.databaseURL}/cleancode/comments.json?auth=${this.props.user.idToken}`,
+      requestOptions
+    )
+      // .then((response) => response.json())
+      // .then((result) => {
+      // 	// CLOSE MODAL AFTER POST
+      // 	window.location.reload();
+      // })
+      .catch((error) => console.log("error:", error));
+    // this.props.getFirebase();
+  }
 
-			{/* CODEBLOCK */}
-			<h4 className="posts__h4">Code:</h4>
-			<span className="codeblock__span">
-				<Codeblock currentPost={props.currentPost} />
-			</span>
+  render() {
+    console.log(this.props);
 
-			{/* USER NAME */}
-			<p className="user__name">{props.currentPost[6]}</p>
-		</div>
-	);
-};
+    return (
+      <>
+        <div className="post__div">
+          <button
+            className="nav__link nav__close"
+            onClick={this.props.reloadPage}
+          >
+            x close
+          </button>
+          {/* <Link className="nav__link nav__close" to="/">
+					x close
+				</Link> */}
+
+          {/* TITLE */}
+          <h3 className="posts__h3">{this.props.currentPost[5]}</h3>
+
+          {/* CATEGORIES */}
+          <h4 className="posts__p">{this.props.currentPost[2]}</h4>
+          <h4 className="posts__p">{this.props.currentPost[4]}</h4>
+
+          {/* ARTICLE */}
+          <p className="posts__p">{this.props.currentPost[1]}</p>
+
+          {/* CODEBLOCK */}
+          <h4 className="posts__h4">Code:</h4>
+          <span className="codeblock__span">
+            <Codeblock currentPost={this.props.currentPost} />
+          </span>
+
+          {/* USER NAME */}
+          <p className="user__name">{this.props.currentPost[6]}</p>
+        </div>
+        {/* COMMENT BLOCK */}
+        <div className="posts__comment ">
+          {/* COMMENT FORM */}
+          {this.props.user ? (
+            <form action="#" className="form__main">
+              <legend className="form__title">
+                Tell us what do you think about this:
+              </legend>
+              <label htmlFor="form__comment" className="form__comment">
+                Comment
+              </label>
+              <textarea
+                onChange={this.textAreaChange}
+                name="comment"
+                id="form__text"
+                cols="50"
+                rows="10"
+                value={this.state.comment}
+              ></textarea>
+              <button className="form__button" onClick={this.addComment}>
+                Add
+              </button>
+            </form>
+          ) : null}
+          {/* COMMENTS HERE IN FUTURE */}
+        </div>
+      </>
+    );
+  }
+}
 
 export default Userpost;
