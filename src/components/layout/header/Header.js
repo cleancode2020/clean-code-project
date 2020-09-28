d89bb5d5ead975de6ed19d8eb240eecd487d024e
Victoria <testversion2617@gmx.de>
2020-09-28 14:31:28 +0200

    change in header logo and font

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../../assets/icons/black-dot.png";
+import logo from "../../../assets/icons/logo.png";
 import "./header.css";
 import { Link } from "react-router-dom";
 import LogIn from "../../authentification/LogIn";
