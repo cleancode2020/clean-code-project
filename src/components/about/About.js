01f48e50febafb418612be4f171f38952a79a2dd
Gael <src.gael@gmail.com>
2020-10-07 15:49:41 +0200

    started cards

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,55 +1,12 @@
 import React from "react";
 import "../about/about.css";
-import gael from "../../assets/teamImage/Gael.jpg";
-import andreas from "../../assets/teamImage/Andreas.png";
-import melad from "../../assets/teamImage/Melad.png";
-import victoria from "../../assets/teamImage/Victoria.jpg";
-import vadim from "../../assets/teamImage/Vadim.png";
+import victoria from "../../assets/teamImage/victoria.jpeg";
+import vadim from "../../assets/teamImage/vadim.jpeg";
+import melad from "../../assets/teamImage/melad.jpeg";
+import andreas from "../../assets/teamImage/andreas.jpeg";
+import gael from "../../assets/teamImage/gael.jpeg";
 
 class About extends React.Component {
-	state = {
-		teams: [
-			{
-				id: 1,
-				name: "Gael",
-				title: "frontend-management",
-				linkedInPage: "https://www.linkedin.com/in/gael-src/",
-				github: "https://github.com/gael-src",
-			},
-			{
-				id: 2,
-				name: "Andreas",
-				title: "frontend-management",
-				linkedInPage: "",
-				github: "https://github.com/jokk3r",
-			},
-			{
-				id: 3,
-				name: "Melad",
-				title: "frontend-developer",
-				linkedInPage: "https://www.linkedin.com/in/melad-kewan-a087221b2/",
-				github: "https://github.com/melkew",
-			},
-			{
-				id: 4,
-				name: "Victoria",
-				title: "frontend-developer",
-				linkedInPage: "https://www.linkedin.com/in/victoria15/",
-				github: "https://github.com/victoria2266",
-			},
-			{
-				id: 5,
-				name: "Vadim",
-				title: "frontend-developer",
-				linkedInPage: "https://www.linkedin.com/in/vadim-eremia-6a846510a/",
-				github: "https://github.com/wadush",
-			},
-		],
-	};
-	handleOnMouseMove = () => {
-		return console.log(this.state.teams[1].title);
-	};
-
 	render() {
 		return (
 			<main className="main__about">
@@ -116,83 +73,117 @@ class About extends React.Component {
 
 				{/* TEAM PICS AND SOME TEXT ABOUT THEM */}
 				<h2 className="h2__style">Our Team</h2>
-				<div className="team__container">
-					<figure className="effect__imgs">
-						<div className="figcaption">
-							<img src={gael} alt="gael" />
-							<h2>{this.state.teams[0].name}</h2>
-							<p className="links">
-								<a href={this.state.teams[0].linkedInPage}>Linkedin</a>
-								<a href={this.state.teams[0].github}>Github</a>
-							</p>
-
-							<div className="description">
-								<p>{this.state.teams[0].name}</p>
-								<p>{this.state.teams[0].title}</p>
-							</div>
+				<ul className="sub__about sub__team">
+					<li>
+						<div className="card__up card__victoria">
+							<img
+								className="card__img"
+								src={victoria}
+								alt="Girl in a jacket"
+							/>
 						</div>
-
-						<div className="figcaption">
-							<img src={andreas} alt="andreass" />
-
-							<h2>{this.state.teams[1].name}</h2>
-							<p className="links">
-								<a href={this.state.teams[1].linkedInPage}>Linkedin</a>
-								<a href={this.state.teams[1].github}>Github</a>
-							</p>
-
-							<div className="description">
-								<p>{this.state.teams[1].name}</p>
-								<p>{this.state.teams[1].title}</p>
-							</div>
+						<div className="card__down">
+							<a
+								href="https://www.linkedin.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								LinkedIn
+							</a>
+							<a
+								href="https://github.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								GitHub
+							</a>
 						</div>
-
-						<div className="figcaption">
-							<img src={melad} alt="melad" />
-
-							<h2>{this.state.teams[2].name}</h2>
-							<p className="links">
-								<a href={this.state.teams[2].linkedInPage}>Linkedin</a>
-								<a href={this.state.teams[2].github}>Github</a>
-							</p>
-
-							<div className="description">
-								<p>{this.state.teams[2].name}</p>
-								<p>{this.state.teams[2].title}</p>
-							</div>
+					</li>
+					<li>
+						<div className="card__up card__vadim">
+							<img className="card__img" src={vadim} alt="Girl in a jacket" />
 						</div>
-
-						<div className="figcaption">
-							<img src={victoria} alt="victoria" />
-
-							<h2>{this.state.teams[3].name}</h2>
-							<p className="links">
-								<a href={this.state.teams[3].linkedInPage}>Linkedin</a>
-								<a href={this.state.teams[3].github}>Github</a>
-							</p>
-
-							<div className="description">
-								<p>{this.state.teams[3].name}</p>
-								<p>{this.state.teams[3].title}</p>
-							</div>
+						<div className="card__down">
+							<a
+								href="https://www.linkedin.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								LinkedIn
+							</a>
+							<a
+								href="https://github.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								GitHub
+							</a>
 						</div>
-
-						<div className="figcaption">
-							<img src={vadim} alt="vadim" />
-
-							<h2>{this.state.teams[4].name}</h2>
-							<p className="links">
-								<a href={this.state.teams[4].linkedInPage}>Linkedin</a>
-								<a href={this.state.teams[4].github}>Github</a>
-							</p>
-
-							<div className="description">
-								<p>{this.state.teams[4].name}</p>
-								<p>{this.state.teams[4].title}</p>
-							</div>
+					</li>
+					<li>
+						<div className="card__up card__melad">
+							<img className="card__img" src={melad} alt="Girl in a jacket" />
 						</div>
-					</figure>
-				</div>
+						<div className="card__down">
+							<a
+								href="https://www.linkedin.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								LinkedIn
+							</a>
+							<a
+								href="https://github.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								GitHub
+							</a>
+						</div>
+					</li>
+					<li>
+						<div className="card__up card__andreas">
+							<img className="card__img" src={andreas} alt="Girl in a jacket" />
+						</div>
+						<div className="card__down">
+							<a
+								href="https://www.linkedin.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								LinkedIn
+							</a>
+							<a
+								href="https://github.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								GitHub
+							</a>
+						</div>
+					</li>
+					<li>
+						<div className="card__up card__gael">
+							<img className="card__img" src={gael} alt="Girl in a jacket" />
+						</div>
+						<div className="card__down">
+							<a
+								href="https://www.linkedin.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								LinkedIn
+							</a>
+							<a
+								href="https://github.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								GitHub
+							</a>
+						</div>
+					</li>
+				</ul>
 			</main>
 		);
 	}
