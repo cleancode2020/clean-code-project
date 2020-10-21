71d065b37706a3be7e958b3f6fda950352ae58fc
Gael <src.gael@gmail.com>
2020-10-21 13:44:03 +0200

    css updates

--- src/components/main/posts/readposts/Codeblockread.js
+++ src/components/main/posts/readposts/Codeblockread.js
@@ -1,14 +1,17 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { atelierLakesideLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
+// import { atelierLakesideLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
 // import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
+import foundation from "react-syntax-highlighter/dist/esm/styles/hljs/foundation";
 
 const Codeblock = (props) => {
 	return (
 		<SyntaxHighlighter
-			language="javascript"
-			style={atelierLakesideLight}
 			className="codeblock"
+			language="javascript"
+			// style={atelierLakesideLight}
+			// style={prism}
+			style={foundation}
 		>
 			{props.currentPost[3]}
 		</SyntaxHighlighter>
--- src/components/main/posts/readposts/submitComment/comments/CodeBlockComment.js
+++ src/components/main/posts/readposts/submitComment/comments/CodeBlockComment.js
@@ -1,16 +1,21 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { atelierLakesideLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
+// import { atelierLakesideLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
+// import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
+import foundation from "react-syntax-highlighter/dist/esm/styles/hljs/foundation";
+
 function CodeBlockComment(props) {
-  return (
-    <SyntaxHighlighter
-      language="javascript"
-      style={atelierLakesideLight}
-      className="codeblock"
-    >
-      {props.code}
-    </SyntaxHighlighter>
-  );
+	return (
+		<SyntaxHighlighter
+			className="codeblock"
+			language="javascript"
+			// style={atelierLakesideLight}
+			// style={prism}
+			style={foundation}
+		>
+			{props.code}
+		</SyntaxHighlighter>
+	);
 }
 
 export default CodeBlockComment;
